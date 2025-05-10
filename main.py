211e379e8940e5891384fcf936a8b8d5f08c7223
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-10 14:45:00 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("hello word")
+if 5 > 2:
+	print("Five is greater than two!")
+if 5 > 2:
+	 print("Five is greater than two!")
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
