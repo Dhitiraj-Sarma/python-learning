9a3195d5066b12e09c7bd94d99db47708baddc22
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-20 19:35:18 +0530

    copy of a array

--- numpy/array-manipulation.py
+++ numpy/array-manipulation.py
@@ -30,3 +30,32 @@ print("view- ", v) # [12  4  6  8 10]
 v[0] = 99
 print("original array- ", arr) # [99  4  6  8 10]
 print("view- ", v) # [99  4  6  8 10]
+
+
+
+# Copy (Deep Copy) of Array in NumPy
+# The copy is completely a new array and the copy owns the data.
+# Any in-place change to the copy will not be reflected in the original array.
+# creating array
+arr1 = np.array([2, 4, 6, 8, 10])
+
+# creating copy of array
+c = arr1.copy()
+
+# both arr and v have different id 
+print("id of arr", id(arr1)) # 2664561984496
+print("id of c", id(c)) # 2664561984592
+
+
+# changing original array
+# this will not effect copy
+arr1[0] = 12
+
+print("original array- ", arr1) # [12  4  6  8 10]
+print("view- ", c) # [ 2  4  6  8 10]
+
+# changing the copy array
+c[0] = 99
+
+print("original array- ", arr1) # [12  4  6  8 10]
+print("view- ", c) # [99  4  6  8 10]
