f365ba86827193b636f0c0861c00bc4cebfde7e2
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-05-23 19:34:03 +0530

    dtype in numpy

--- numpy/numpy-youtube.py
+++ numpy/numpy-youtube.py
@@ -14,7 +14,13 @@ print(arr_2d.size) # 6
 
 print(arr_2d.ndim) # 2
 
+#  How to check data-type
+
+print(arr_2d.dtype) # int64
+
 arr = np.array([[[1, 2], [3, 4], [5, 6], [7, 8]]])
 print(arr)
 print(arr.ndim)
 print(arr.shape)
+
+# changing type of a array
