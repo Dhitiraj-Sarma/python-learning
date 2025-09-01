3d94981d68b888f0d1aaf508559cf619ae10e2f6
Dhitiraj Sarma <dhitirajsarma66@gmail.com>
2025-09-01 14:57:32 +0530

    pandas

---
+++ pandas/course/describe.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = {
+	"Name" : ['Ram', 'Shayam', 'Ghanshyam', 'Dhanshyam', 'Aditi', 'Jagdish', 'Raj', 'Simran'],
+	"Age" : [10, 20 , 30, 40, 50,60, 70, 80],
+	"Salary" : [10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000],
+	"Performance Score" : [85, 90, 75, 92, 88, 95, 80, 89]
+}
+
+df = pd.DataFrame(data)
+
+print('Descriptive Staristics')
+print(df.describe())
--- pandas/course/rows.py
+++ pandas/course/rows.py
@@ -2,3 +2,20 @@ import pandas as pd
 
 # head() tail()
 
+# df=pd.read_json("sales.json")
+# print('Dispaly 3 rows of first')
+# print(df.head(3))
+# print('Display 3 rows of last')
+# print(df.tail(3))
+
+
+# info() method
+# 1 - number of rows and columns
+# 2-column name
+# 3- int64 float64 object
+# non null counts
+# memory usage of the data frame
+
+# df = pd.read_json("sales_sample.json")
+# print("Displaying the info of data set")
+# print(df.info())
